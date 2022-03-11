ad028129097820bf0057336e8cfb477e01786971
ignitedevua <ignitedevua@gmail.com>
2022-03-11 17:18:33 +0200

    sh

---
+++ alive_install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+main() {
+    git_url="${1}"
+    local_folder="alive"
+
+    echo "Check python3 ..."
+    which python3 || exit 1
+    echo "OK"
+
+    echo "Cloning repo ..."
+    git clone "${git_url}" "${local_folder}" || exit 1
+    echo "OK"
+    
+    python3 -m venv "${local_folder}"
+
+    cd "${local_folder}"
+    echo "Install deps ... "
+    pip3 install -r requirements.txt  &> /dev/null
+    echo "OK"
+
+    echo "Create config ..."
+    cat <<EOF > "config.ini"
+[Alive]
+git = ${git_url}
+EOF
+    echo "OK"
+
+    chmod +x "main.py"
+    echo "Scheduling cron ..."
+    crontab -l > cron_bkp
+    echo "*/15 * * * * cd "$(pwd)"; ./main.py >/dev/null 2>&1" >> cron_bkp
+    crontab cron_bkp
+    rm cron_bkp
+    echo "OK"
+}
+
+main "${@}"
